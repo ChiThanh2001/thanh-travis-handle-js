96c2ba51155c8c3e51f68e8e50234c852856da79
ChiThanh2001 <satthumaulanh2001@gmail.com>
2023-02-11 21:49:54 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const findMedianSortedArrays = require('./Median_of_2_sorted_arrays')
 const reverseInt = require('./reverse_integer')
 const romanToInt = require('./roman_to_integer')
-g
+
 module.exports = {
     findMedianSortedArrays,
     reverseInt,
